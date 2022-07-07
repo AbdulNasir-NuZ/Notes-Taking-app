e805c8bfd9031ebb03690b8d2b5c8c85f88cf0ae
Abdul nasir V <108569716+AbdulNasir-NuZ@users.noreply.github.com>
2022-07-07 13:47:58 +0530

    add projects via upload
    first Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> Note </h1>
+      <p>"Success is not that Easy"</p>
+    </div>
+  );
+}
+export default Notes;
